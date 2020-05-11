6a4a3c254adcb0e2c61435c74076d255b9397f59
M Arief Habibirrahman <44163185+habibirrahman@users.noreply.github.com>
2020-05-11 22:56:42 +0700

    Update array.php

--- Taks 1/array.php
+++ Taks 1/array.php
@@ -1,14 +1,7 @@
 <?php
-# random array int 
 $arr = [5, 5, 1, 6, 4, 3];
-# desc sort array's element 
-rsort($arr);
-# get second largest element
-$large_2 = 0;
-foreach ($arr as $val) {
-    if ($arr[0] > $val) {
-        $large_2 = $val;
-        break;
-    }
-}
-print("second largest number is ".$large_2);
+
+$num = array_diff($arr, [max($arr)]);
+$num = max($num);
+
+print("output: ".$num);
